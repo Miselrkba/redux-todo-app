233ba0f7ae5b45c2e24c09269518387e7c624c48
iii <miselrkba@gmail.com>
2020-11-11 11:31:27 +0100

    added createSlice for reducers and actions

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,14 +1,52 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 let todoId = 1;
 
-export const slice = createSlice = ({
-    name: 'todos',
-    initialState: [],
-    reducers: {
-        create: (state, action) => {
-            const {payload} = action;
-            
-        }
-    }
-})
+export const slice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    create: (state, action) => {
+      const { payload } = action;
+
+      state.push({
+        description: payload,
+        id: todoId,
+        isComplete: false,
+      });
+
+      todoId++;
+    },
+    edit: (state, action) => {
+      const { id, description } = action.payload;
+
+      const todoToEdit = state.find((todo) => todo.id === id);
+
+      if (todoToEdit) {
+        todoToEdit.description = description;
+      }
+    },
+    toggleComplete: (state, action) => {
+      const { payload } = action;
+
+      const todoToToggle = state.find((todo) => todo.id === payload);
+
+      if (todoToToggle) {
+        todoToToggle.isComplete = !todoToToggle.isComplete;
+      }
+    },
+    remove: (state, action) => {
+      const { payload } = action;
+
+      const index = state.findIndex((todo) => todo.id === payload);
+
+      if (index === -1) {
+        state.splice(index, 1);
+      }
+    },
+  },
+});
+
+export const {create, edit, toggleComplete, remove} = slice.actions
+
+export default slice.reducer
